8b2ffe950ad4afa980619fca0c78787e621a4dcc
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-07-27 17:24:53 +0200

    Selfwork Livewire

---
+++ livewire_1_vincenzo_galione/resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+
+    <header class="header">
+        <div class="container h-100">
+            <div class="row justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-6 mt-5 ">
+                    <h1 class="text-center">Livewire</h1>
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+            <livewire:counter/>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
